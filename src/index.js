5b00cc65bdce3779ca00add1969bc3a870853fd5
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-05-15 17:12:20 -0300

    feat: implementação do middleware checksTodoExists

--- src/index.js
+++ src/index.js
@@ -40,6 +40,29 @@ function checksCreateTodosUserAvailability(request, response, next) {
 function checksTodoExists(request, response, next) {
   const { username } = request.headers;
   const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  const validateId = validate(id);
+
+  if(!validateId) {
+    return response.status(400).json({ error: 'Invalid id' });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) {
+    return response.status(404).json({ error: 'Todo not found' });
+  }
+
+  request.todo = todo;
+  request.user = user;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
