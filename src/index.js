ee837f74af1952041acf507df66bbdaf50849641
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-05-15 16:41:48 -0300

    feat: implementação do middleware checkExistsUserAccount

--- src/index.js
+++ src/index.js
@@ -10,7 +10,17 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
@@ -18,7 +28,8 @@ function checksCreateTodosUserAvailability(request, response, next) {
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
 }
 
 function findUserById(request, response, next) {
